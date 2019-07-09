c8b1cd0c5e3d2bc7909cf4dd3244cde6b3c06825
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-07-09 01:17:35 -0700

    create function to retrieve a films data from given id

---
+++ server/database.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+const loginInfo = {
+  user: 'root',
+  password: '',
+  database: 'featurefilm',
+}
+
+const connection = mysql.createConnection(loginInfo);
+
+const getFeaturedFilmById = (id, callback) => {
+  connection.query(`SELECT * FROM Features WHERE id = ${id}`, (err, result) => {
+    if(err) console.log(err);
+    callback(err, result)
+  });
+}
+
+module.exports = {
+  getFeaturedFilmById,
+}
