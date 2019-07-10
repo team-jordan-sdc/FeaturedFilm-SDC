182ba4be7f8d9ef4747ac63854ab51ba2680e9a2
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-07-10 16:45:43 -0700

    adding general stats and ratings styling

--- client/src/component/filmStats.jsx
+++ client/src/component/filmStats.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+// TODO populate with data from server
+let num = 3.5;
+let starWidth = 14.6 * num;
+let divStyle={
+  width: starWidth + 'px',
+}
 
 const MovieStats = () => (
   <div>
     <div className="generalStats">
-      test1
+      <span className="stat date">Apr 5</span>
+      <span className="stat important">NC-17</span>
+      <span className="stat important">9000 min</span>
+      <span className="stat important">CC</span>
     </div>
     <div className="ratings">
-      test2
+      <div className="starzone">
+        <div className="nostars">
+          <div className="stars" style={divStyle}></div>
+        </div>
+      </div>
     </div>
 
   </div>
