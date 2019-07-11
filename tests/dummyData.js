c7715376bcb020c39c0ce966fe0e0d45332d2392
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-07-11 16:52:22 -0700

    dummy data to run tests with

---
+++ tests/dummyData.js
@@ -0,0 +1,23 @@
+
+
+const dummyData = {
+  category_1: "fantasy",
+  category_2: "action",
+  description: "Cillum eu elit minim Lorem. Aliquip anim aliqua ea minim laboris ipsum dolor dolore.↵Ipsum sint id in Lorem consequat sunt consequat. Ut dolore exercitation et adipisicing. Ullamco eu quis do ea Lorem in. Culpa tempor esse sit commodo velit eu. Laboris nulla sint fugiat laboris cillum veniam sint cillum reprehenderit.",
+  hd_rent: 1235,
+  hs_cost: 1265,
+  id: 6,
+  length: 177,
+  movie_cover_url: "https://fec1-arwen-featuredfilms.s3-us-west-2.amazonaws.com/mudoo+posters/3-168.jpeg",
+  movie_shot_url: "https://fec1-arwen-featuredfilms.s3-us-west-2.amazonaws.com/mudoo+backgrounds/3-1280a.jpg",
+  mpaa_rating: "G",
+  release_date: 2019,
+  rt_rating: 91,
+  sd_cost: 531,
+  sd_rent: 3,
+  star_rating: 205,
+  star_rating_count: 2618,
+  title: "The Lord of the Rings: The Return of the King",
+}
+
+export default dummyData;
