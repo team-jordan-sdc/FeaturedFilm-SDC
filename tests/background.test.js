7a3b76939fc26d849c606e8f1d5ee31ce360e623
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-07-11 16:52:52 -0700

    a few tests for background component

--- tests/background.test.js
+++ tests/background.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Enzyme, { shallow, mount } from 'enzyme';
 import backgroundImage from '../client/src/component/background.jsx';
 import Adapter from 'enzyme-adapter-react-16';
+import dummyData from './dummyData.js'
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -9,5 +10,10 @@ describe('enzyme test of background', () => {
   test('renders', ()=>{
     const wrapper = shallow(<backgroundImage />)
     expect(wrapper.exists()).toBe(true);
-  })
-})
+  });
+
+  test('check and make sure props are being sent', () => {
+    const wrapper = shallow(<backgroundImage film={dummyData}/>);
+    expect(wrapper.props().film.movie_shot_url).toEqual('https://fec1-arwen-featuredfilms.s3-us-west-2.amazonaws.com/mudoo+backgrounds/3-1280a.jpg')
+  });
+});
