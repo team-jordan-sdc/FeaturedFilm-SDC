bd24db1bd9a4d1469d9cec85ab33bb5916831f99
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-07-12 15:04:36 -0700

    moved app to its own seperate file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,40 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BackgroundImage from './component/background.jsx';
+import App from './app.jsx';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      film: {},
-    }
-    this.fetchRandFeaturedFilm = this.fetchRandFeaturedFilm.bind(this);
-  }
-
-  componentDidMount(){
-    console.log('fetch random film');
-    this.fetchRandFeaturedFilm();
-  }
-
-  fetchRandFeaturedFilm(){
-    fetch('/api/rand')
-    .then(res =>res.json());
-    .then((res) => {
-      this.setState({film: res[0]});
-    });
-  }
-
-
-  render(){
-    return(
-      <div>
-        <BackgroundImage film={this.state.film}/>
-      </div>
-    );
-  }
-
-
-
-}
 
 ReactDOM.render(<App />, document.getElementById('test'));
