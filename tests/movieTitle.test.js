f2703716792a446a0789a201d7aebb961aa1eaf4
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-07-11 20:06:21 -0700

    add tests for title component

---
+++ tests/movieTitle.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Enzyme, { shallow, mount } from 'enzyme';
+import MovieTitle from '../client/src/component/movieTitle.jsx';
+import Adapter from 'enzyme-adapter-react-16';
+import dummyData from './dummyData.js';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('tests for title component', () => {
+  test('check if title renders', () => {
+    const wrapper = shallow(<MovieTitle title={dummyData.title}/>);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  test('check if title changes', () => {
+    const wrapper = shallow(<MovieTitle title={dummyData.title}/>);
+    expect(wrapper.find('.movieTitle').text()).toEqual('The Lord of the Rings: The Return of the King');
+  });
+});
