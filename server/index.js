7360309838e868ecd436af96f3b58544e026dd55
mmcphillips <matthewcmcphillips@gmail.com>
2019-07-24 20:47:52 -0700

    Implement functionality for create, update, and delete routes

--- server/index.js
+++ server/index.js
@@ -5,35 +5,53 @@ const db = require('./database.js');
 
 
 const app = express();
-
+app.use(bodyparser.json());
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 app.use('*.js', (req, res, next) => {
-  req.url = req.url + '.gz';
+  req.url += '.gz';
   res.set('Content-Encoding', 'gzip');
   next();
 });
 
 app.get('/api/rand', (req, res) => {
-  let randNum = Math.floor(Math.random() * 99) + 1;
+  const randNum = Math.floor(Math.random() * 99) + 1;
   db.getFeaturedFilmById(randNum, (err, result) => {
     res.status(200).send(result);
   });
 });
 
+// read-
 app.get('/api/featured', (req, res) => {
-  let id = req.query.id;
-  db.getFeaturedFilmById(id, (err, result) => {
-    res.status(200).send(result);
-  });
+  const { id } = req.query;
+  db.getFeaturedFilmById(id)
+    .then(result => res.send(result).status(201).end())
+    .catch(error => console.log('An error occurred:', error));
 });
 
-app.post('/api/rate', (req, res) => {
-  res.status(200).end();
+// create
+app.post('/api/featured', (req, res) => {
+  const film = req.body.filmInfo;
+  db.createFeaturedFilm(film)
+    .then(result => res.send(result).status(201).end())
+    .catch(error => console.log('An error occurred:', error));
 });
 
-app.post('/api/wishlist', (req, res) => {
-  res.status(200).end();
+// update
+app.put('/api/featured', (req, res) => {
+  const film = req.body.filmInfo;
+  db.updateFeaturedFilm(film)
+    .then(result => res.send(result).status(405).end())
+    .catch(error => console.log('An error occurred:', error));
 });
+
+// delete
+app.delete('/api/featured', (req, res) => {
+  const { id } = req.body;
+  db.removeFeaturedFilm(id)
+    .then(result => res.send(result).status(405).end())
+    .catch(error => console.log('An error occurred:', error));
+});
+
 
 module.exports = app;
