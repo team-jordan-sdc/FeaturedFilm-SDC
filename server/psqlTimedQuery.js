b54122182fec69af1729452e0e965b41f1ccd7c2
Matthew McPhillips <matthewcmcphillips@gmail.com>
2019-07-31 19:04:28 -0700

    Implement function to test query speeds of end of database end.

---
+++ server/psqlTimedQuery.js
@@ -0,0 +1,23 @@
+const Pool = require('pg').Pool;
+require('dotenv').config();
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PG,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
+});
+
+const searchTableQuery =
+`select (title, category_1, description) from psqlFeatures where id=10000000;`;
+
+const psqlEndQuery = function() {
+  console.time('endQuery');
+  pool.query(searchTableQuery)
+  // .then((results) => console.log(results))
+  .then(() => console.timeEnd('endQuery'))
+  .catch((err) => console.log(err));
+}
+
+psqlEndQuery();
