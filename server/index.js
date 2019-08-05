c42e462f1ac768afe2ffef12a17baa5b07b38f80
Matthew McPhillips <matthewcmcphillips@gmail.com>
2019-08-05 15:29:37 -0700

    Implement routes for psql database queries.

--- server/index.js
+++ server/index.js
@@ -1,57 +1,87 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
 const bodyparser = require('body-parser');
-const db = require('./database.js');
-
+const cors = require('cors');
+const pool = require('./psqldb.js');
 
 const app = express();
+app.use(cors());
 app.use(bodyparser.json());
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 app.use('*.js', (req, res, next) => {
-  req.url += '.gz';
+  req.url += '.js';
   res.set('Content-Encoding', 'gzip');
   next();
 });
 
-app.get('/api/rand', (req, res) => {
-  const randNum = Math.floor(Math.random() * 99) + 1;
-  db.getFeaturedFilmById(randNum, (err, result) => {
-    res.status(200).send(result);
+// read-
+app.get('/api/featured', (request, response) => {
+  const { id } = request.query;
+  const getQuery = 'SELECT * FROM features WHERE id = $1';
+  pool
+    .query(getQuery, [id], (error, results) => {
+      if (error) {
+        response.status(404).end();
+      }
+      response.status(200).json(results.rows).end();
+    });
+});
+
+// create-
+app.post('/api/featured', (request, response) => {
+  const { film } = request.body;
+  const createQuery = `INSERT INTO features(
+    title,
+    category_1,
+    category_2,
+    release_date,
+    mpaa_rating,
+    length,
+    star_rating,
+    star_rating_count,
+    rt_rating,
+    description,
+    hd_rent,
+    sd_rent,
+    hs_cost,
+    sd_cost,
+    movie_shot_url,
+    movie_cover_url
+  ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)`;
+  pool.query(createQuery, film, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(201).json(results.rows).end();
   });
 });
 
-// read-
-app.get('/api/featured', (req, res) => {
-  const { id } = req.query;
-  db.getFeaturedFilmById(id)
-    .then(result => res.send(result).status(201).end())
-    .catch(error => console.log('An error occurred:', error));
-});
-
-// create
-app.post('/api/featured', (req, res) => {
+// update-
+app.put('/api/featured', (request, response) => {
   const { film } = req.body;
-  db.createFeaturedFilm(film)
-    .then(result => res.send(result).status(201).end())
-    .catch(error => console.log('An error occurred:', error));
+  const updateQuery = 'UPDATE features SET $1 = $2 WHERE id = $3';
+  pool
+    .query(updateQuery, film, (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows).end();
+    });
 });
 
-// update
-app.put('/api/featured', (req, res) => {
-  const { film } = req.body;
-  db.updateFeaturedFilm(film)
-    .then(result => res.send(result).status(405).end())
-    .catch(error => console.log('An error occurred:', error));
+// delete-
+app.delete('/api/featured', (request, response) => {
+  const { id } = request.body;
+  const deleteQuery = 'DELETE FROM features WHERE id = $1;';
+  pool
+    .query(deleteQuery, [id], (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.send(`Deletion successful at id ${id}`).status(405).json(results.rows).end();
+    });
 });
-
-// delete
-app.delete('/api/featured', (req, res) => {
-  const { id } = req.body;
-  db.removeFeaturedFilm(id)
-    .then(result => res.send(result).status(405).end())
-    .catch(error => console.log('An error occurred:', error));
-});
-
 
 module.exports = app;
