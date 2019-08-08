cdc4b1ee55c38f8d6201982cba5a7f592c33c3ff
Matthew McPhillips <matthewcmcphillips@gmail.com>
2019-08-08 11:19:02 -0700

    Refactor helper functions and add functionality for new seeding method

--- server/helperFunctions.js
+++ server/helperFunctions.js
@@ -2,10 +2,17 @@ const { LoremIpsum } = require('lorem-ipsum');
 const { movies } = require('./movieList.js');
 
 const bgPath = 'https://featurefilm.s3-us-west-2.amazonaws.com/bg/wudu-background-';
+
 const featurePath = 'https://featurefilm.s3-us-west-2.amazonaws.com/f/wudu-feature-';
 const tagsArray = ['comedy', 'horror', 'drama', 'holiday special', 'action', 'adventure', 'scifi', 'fantasy', 'thriller', 'animated'];
 const mpaaRatings = ['G', 'PG', 'PG-13', 'R', 'NC-17', 'Unrated'];
 const movieNamesArray = movies.split('\n');
+const getRandomNumberTo6 = function () {
+  return Math.floor(Math.random() * 6) + 1;
+};
+const getRandomNumberTo10 = function () {
+  return Math.floor(Math.random() * 10) + 1;
+};
 const getRandomNumberTo100 = function () {
   return Math.floor(Math.random() * 100) + 1;
 };
@@ -39,6 +46,8 @@ module.exports = {
   tagsArray,
   mpaaRatings,
   movieNamesArray,
+  getRandomNumberTo6,
+  getRandomNumberTo10,
   getRandomNumberTo100,
   getRandomNumberTo200,
   getRandomNumberTo500,
