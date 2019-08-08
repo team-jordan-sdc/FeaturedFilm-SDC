aa55e0150d9fd7aca659fa4f5c1deb955b88a94c
Matthew McPhillips <matthewcmcphillips@gmail.com>
2019-08-08 13:46:06 -0700

    Implement missed query.

--- server/index.js
+++ server/index.js
@@ -18,7 +18,41 @@ app.use('*.js', (req, res, next) => {
 
 app.get('/api/featured', (request, response) => {
   const { id } = request.query;
-  const getQuery = 'SELECT * FROM features WHERE id = $1';
+  const getQuery = `
+  SELECT
+    category1.category as category1,
+    category2.category as category2,
+    titles.title,
+    bgpath.bgurl,
+    fpath.featureurl,
+    mpaaratings.rating,
+    features.description,
+    features.length,
+    features.release_date,
+    features.rt_rating,
+    features.star_rating,
+    features.star_rating_count,
+    features.hs_cost,
+    features.hd_rent,
+    features.sd_cost,
+    features.sd_rent,
+    features.movie_shot_url,
+    features.movie_cover_url
+    FROM features
+    LEFT JOIN titles ON
+    features.title=titles.id
+    LEFT JOIN categories AS category1 ON
+    features.category_1=category1.id
+    LEFT JOIN categories AS category2 ON
+    features.category_2=category2.id
+    LEFT JOIN mpaaratings ON
+    features.mpaa_rating=mpaaratings.id
+    LEFT JOIN bgpath ON
+    features.movie_cover_url=bgpath.id
+    LEFT JOIN fpath ON
+    features.movie_shot_url=fpath.id
+    WHERE features.id = $1;
+    `;
   pool
     .query(getQuery, [id], (error, results) => {
       if (error) {
